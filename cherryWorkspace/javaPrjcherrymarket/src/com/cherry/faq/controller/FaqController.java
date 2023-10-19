c7b02eec94e38570b58ec3026ba1a86f68ced4a5
Kwangs <leekp7@naver.com>
2023-10-19 15:27:02 +0900

    [패키지,클래스 파일 생성] 작성자 : 양덕기 Main FaqController FaqService FaqDao 게시판 생성 코드 작성중, main에 scanner, loginmember 추가 일자 : 2023.10.19

--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/faq/controller/FaqController.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/faq/controller/FaqController.java
@@ -1,5 +1,55 @@
 package com.cherry.faq.controller;
 
+import com.cherry.faq.service.FaqService;
+import com.cherry.faq.vo.FaqVo;
+import com.cherry.main.Main;
+
 public class FaqController {
 
-}
+	//필드
+	private final FaqService fs;
+
+	//기본생성자
+	public FaqController() {
+		fs = new FaqService();
+	}
+	
+	//게시판 등록
+	public void write() {
+		try {
+			System.out.println("-----게시판 등록-----");
+			//데이터
+			System.out.print("제목: ");
+			String title = Main.SC.nextLine();
+			System.out.println("내용: ");
+			String content = Main.SC.nextLine();
+			
+			FaqVo vo = new FaqVo();
+			vo.setTitle(title);
+			vo.setContent(content);
+			
+			//서비스
+			int result = fs.write();
+			
+			//결과 처리
+			if(result!= 1) {
+				throw new Exception();
+			}
+			System.out.println("게시판 등록 성공!");
+			
+		}catch(Exception e) {
+			System.out.println("게시판 등록 실패..");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	//게시판 수정
+	
+	//게시판 삭제(update del_yn)
+	
+	//게시판 조회(no기본키로)
+	
+	//게시판 전체 조회(관리자만)
+	
+}//class
--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/faq/vo/FaqVo.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/faq/vo/FaqVo.java
@@ -2,4 +2,82 @@ package com.cherry.faq.vo;
 
 public class FaqVo {
 
+	private String faqNo;
+	private String managerNo;
+	private String title;
+	private String content;
+	private String enrollDate;
+	private String secretYn;
+	private String editDate;
+	
+	//기본 생성자
+	public FaqVo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	//생성자
+	public FaqVo(String faqNo, String managerNo, String title, String content, String enrollDate, String secretYn,
+			String editDate) {
+		super();
+		this.faqNo = faqNo;
+		this.managerNo = managerNo;
+		this.title = title;
+		this.content = content;
+		this.enrollDate = enrollDate;
+		this.secretYn = secretYn;
+		this.editDate = editDate;
+	}
+	
+	public String getFaqNo() {
+		return faqNo;
+	}
+	public void setFaqNo(String faqNo) {
+		this.faqNo = faqNo;
+	}
+	public String getManagerNo() {
+		return managerNo;
+	}
+	public void setManagerNo(String managerNo) {
+		this.managerNo = managerNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getEnrollDate() {
+		return enrollDate;
+	}
+	public void setEnrollDate(String enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+	public String getSecretYn() {
+		return secretYn;
+	}
+	public void setSecretYn(String secretYn) {
+		this.secretYn = secretYn;
+	}
+	public String getEditDate() {
+		return editDate;
+	}
+	public void setEditDate(String editDate) {
+		this.editDate = editDate;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "FaqVo [faqNo=" + faqNo + ", managerNo=" + managerNo + ", title=" + title + ", content=" + content
+				+ ", enrollDate=" + enrollDate + ", secretYn=" + secretYn + ", editDate=" + editDate + "]";
+	}
+	
+	
 }
