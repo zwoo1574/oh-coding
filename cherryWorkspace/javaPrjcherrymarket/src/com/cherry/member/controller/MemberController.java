b27d8d385343c8d838409fe40dcf507e7ce38673
DESKTOP-2G50SHI\user1 <corey5361@nate.com>
2023-10-19 15:16:18 +0900

    [member 기능 ]  작성자 : 양덕기 memberController / service / dao / vo 회원가입 로그인 로그아웃 회원탈퇴 비밀번호 변경(미완)

--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/member/controller/MemberController.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/member/controller/MemberController.java
@@ -1,5 +1,110 @@
 package com.cherry.member.controller;
 
-public class MemberController {
+import com.cherry.main.Main;
+import com.cherry.member.service.MemberService;
+import com.cherry.member.vo.MemberVo;
 
+public class MemberController {
+	
+private final MemberService service;
+	
+	public MemberController() {
+		service = new MemberService();
+	}
+	
+	public void selectMemu() {
+		System.out.println("===== 메뉴선택 =====");
+		
+		System.out.println("1: 회원가입 2: 로그인 3: 로그아웃 4: 회원탈퇴");
+		String num = Main.SC.nextLine();
+		
+		switch(num) {
+		case "1" :join(); break;
+		case "2" :login(); break;
+		case "3" :logout(); break;
+		case "4" :quit(); break;
+		}
+	}
+
+	public void join() {
+
+		System.out.println("===== 회원가입 ======");
+		try {
+			System.out.println("ID : ");
+			String id = Main.SC.nextLine();
+			System.out.println("PWD : ");
+			String pwd = Main.SC.nextLine();
+			System.out.println("NICK : ");
+			String nick = Main.SC.nextLine();
+			System.out.println("NAME : ");
+			String name = Main.SC.nextLine();
+			System.out.println("EMAIL : ");
+			String email = Main.SC.nextLine();
+			System.out.println("PHONE : ");
+			String phone = Main.SC.nextLine();
+			System.out.println("ADRESS : ");
+			String adress = Main.SC.nextLine();
+			
+			MemberVo vo = new MemberVo();
+			vo.setId(id);
+			vo.setPwd(pwd);
+			vo.setNick(nick);
+			vo.setName(name);
+			vo.setEmail(email);
+			vo.setPhone(phone);
+			vo.setAddress(adress);
+			
+			int result = service.join(vo); 
+			
+			if(result != 1) {
+				throw new Exception();
+			}
+				System.out.println("회원가입 성공");
+			
+		}catch(Exception e) {
+			System.out.println("회원가입 실패");
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	public void login() {
+		System.out.println("===== 로그인 ======");
+		try {
+			System.out.println("ID : ");
+			String id = Main.SC.nextLine();
+			System.out.println("PWD : ");
+			String pwd = Main.SC.nextLine();
+			
+			MemberVo vo = new MemberVo();
+			vo.setId(id);
+			vo.setPwd(pwd);
+			
+			Main.loginMember = service.login(vo); 
+			
+			if(Main.loginMember == null) {
+				throw new Exception();
+			}
+			System.out.println("로그인 성공");
+		}catch(Exception e) {
+			System.out.println("로그인 실패");
+			e.printStackTrace();
+		}
+	}
+	public void logout() {
+		System.out.println("===== 로그아웃 ======");
+		if(Main.loginMember == null) {
+			System.out.println("로그인 상태가 아닙니다");
+			return;
+		}
+		Main.loginMember = null;
+		System.out.println("로그아웃 되었습니다.");
+		
+	}
+
+	public void quit() {
+		
+		
+	}
 }
--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/member/dao/MemberDao.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/member/dao/MemberDao.java
@@ -1,5 +1,77 @@
 package com.cherry.member.dao;
 
+import java.sql.*;
+
+import com.cherry.jdbc.JDBCTemplate;
+import com.cherry.member.vo.MemberVo;
+
 public class MemberDao {
+
+
+	//회원가입
+	public int join(Connection conn, MemberVo vo) throws Exception{
+		
+		String sql = "INSERT INTO MEMBER(MEMBER_NO,AREAS_CODE,ID,PWD,NICK,NAME,EMAIL,PHONE,ADDRESS)"
+				+ "VALUES (SEQ_MEMBER_MINI.NEXTVAL,?,?,?,?,?,?,?,?)";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, vo.getAreasCode());
+		pstmt.setString(2, vo.getId());
+		pstmt.setString(3, vo.getPwd());
+		pstmt.setString(4, vo.getNick());
+		pstmt.setString(5, vo.getName());
+		pstmt.setString(6, vo.getEmail());
+		pstmt.setString(7, vo.getPhone());
+		pstmt.setString(8, vo.getAddress());
+		
+		int result = pstmt.executeUpdate();
+		JDBCTemplate.close(pstmt);
+		
+		return result;
+	}
+
+	//회원가입에 쓸 주소 -> 동네코드 추출 (예)서울시강남구신사동 -> 신사동
+	public String codeMake(Connection conn, String address) throws Exception{
+		int f = address.indexOf("구");
+		int e = address.indexOf("동");
+		String code = address.substring(f+1);
+		String sql = "SELECT AREAS_CODE FROM AREAS WHERE AREAS_NAME LIKE ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, code);
+		ResultSet rs = pstmt.executeQuery();
+		String areasCode = null;
+		if(rs.next()) {
+			areasCode = rs.getString("AREAS_CODE");			
+		}
+		JDBCTemplate.close(rs);
+		JDBCTemplate.close(pstmt);
+		
+		return areasCode;
+	}
+
+	public MemberVo login(Connection conn, MemberVo vo) throws Exception{
+		String sql = "SELECT * FROM MEMBER WHERE ID = ? AND PWD = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1,vo.getId());
+		pstmt.setString(2, vo.getPwd());
+		ResultSet rs = pstmt.executeQuery();
+		MemberVo userVo= null;
+		if(rs.next()) {
+			userVo = new MemberVo();
+			userVo.setMemberNo(rs.getString("MEMBER_NO"));
+			userVo.setAreasCode(rs.getString("AREAS_CODE"));
+			userVo.setId(rs.getString("ID"));
+			userVo.setPwd(rs.getString("PWD"));
+			userVo.setNick(rs.getString("NICK"));
+			userVo.setName(rs.getString("NAME"));
+			userVo.setEmail(rs.getString("EMAIL"));
+			userVo.setPhone(rs.getString("PHONE"));
+			userVo.setAddress(rs.getString("ADDRESS"));
+			userVo.setJoinDate(rs.getString("JOIN_DATE"));
+			userVo.setEditDate(rs.getString("EDIT_DATE"));
+		}
+		JDBCTemplate.close(pstmt);
+		
+		return userVo;
+	}
 
 }
