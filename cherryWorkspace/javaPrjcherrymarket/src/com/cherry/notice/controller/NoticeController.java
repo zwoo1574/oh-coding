764e5215336258eda5b1a3c3d447473e191bae79
moooook\moook <lolipop6266@gmail.com>
2023-10-19 15:26:40 +0900

    [패키치,클래스 파일 생성] 작성자 : 이현묵 NoticeController NoticeService NoticeDao NoticeVo 패키지만듦

--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/notice/controller/NoticeController.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/notice/controller/NoticeController.java
@@ -1,5 +1,70 @@
 package com.cherry.notice.controller;
 
+import com.cherry.notice.service.NoticeService;
+import com.cherry.notice.vo.NoticeVo;
+
 public class NoticeController {
+//	//필드
+//	private final NoticeService service;
+//	
+//	//기본 생성자
+//		public NoticeController() {
+//			service=new NoticeService();
+//		}
+//	//메뉴선택
+//		public void selectMenu()  {
+//			System.out.println("===NOTICE===");
+//			
+//			System.out.println("1. 공지글 작성");
+//			System.out.println("2. 공지글 수정");
+//			System.out.println("2. 공지글 조회(최신순)");
+//			System.out.println("3. 공지글 상세조회(번호)");
+//			System.out.println("4. 공지글 삭제(관리자만)");
+//			
+//			
+//			String num=Main.SC.nextLine();
+//			switch(num) {
+//			case "1": write(); break;
+////			case "2": modify(); break;
+////			case "3": noticeList(); break;
+////			case "4": noticeDetailByNo(); break;
+////			case "5": delete(); break;
+//			default:System.out.println("잘못입력"); 
+//			}
+//		}
+//	public void write() {
+//		System.out.println("===게시글 작성===");
+//		try {
+//		
+//		//로그인 여부 체크
+//		if(Main.loginMember==null) {
+//			throw new Exception("회원만 게시글 작성이 가능합니다.");
+//		}
+//		
+//			데이터
+//			System.out.print("제목: ");
+//			String title=Main.SC.nextLine();
+//			System.out.print("내용: ");
+//			String content=Main.SC.nextLine();
+//			
+//			NoticeVo vo= new NoticeVo();
+//			vo.setTitle(title);
+//			vo.setContent(content);
+//			
+//			//서비스
+//			int result=service.write(vo);
+//			
+//			//결과
+//			if(result!=1){
+//				throw new Exception();
+//			}
+//			System.out.println("게시글 작성");
+//		}catch(Exception e) {
+//			System.out.println("작성 실패");
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//	
 
 }
--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/notice/service/NoticeService.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/notice/service/NoticeService.java
@@ -1,5 +1,13 @@
 package com.cherry.notice.service;
 
+import com.cherry.notice.vo.NoticeVo;
+
 public class NoticeService {
+
+	public int write(NoticeVo vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 
 }
