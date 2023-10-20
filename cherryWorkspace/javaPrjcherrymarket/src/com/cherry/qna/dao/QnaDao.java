0f4ad06a93065a440c55f9a624cd03e8324bf38b
MJ <douob55@gmail.com>
2023-10-20 11:11:15 +0900

    [기능추가] 작성자 : 이미진
    QnaController
    QnaService
    QnaDao
    QnaVo
    각 클래스에 문의글 작성, 문의글 목록, 문의글 상세조회(번호)
    기능추가

--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/qna/dao/QnaDao.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/qna/dao/QnaDao.java
@@ -2,6 +2,9 @@ package com.cherry.qna.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.cherry.jdbc.JDBCTemplate;
 import com.cherry.main.Main;
@@ -13,11 +16,11 @@ public class QnaDao {
 	public int write(Connection conn, QnaVo vo) throws Exception {
 		
 		//sql
-		String sql = "INSERT INTO BOARD(NO, TITLE, CONTENT, WRITER_NO, SECRET_YN) VALUES(SEQ_BOARD_NO.NEXTVAL,?,?,?,?)";
+		String sql = "INSERT INTO QNA(QNA_NO, TITLE, CONTENT, MEMBER_NO, SECRET_YN) VALUES(SEQ_QNA_NO.NEXTVAL,?,?,?,?)";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setString(1, vo.getTitle());
 		pstmt.setString(2, vo.getContent());
-//		pstmt.setString(3, Main.loginMember.getNo());
+		pstmt.setString(3, Main.loginMember.getMemberNo());
 		pstmt.setString(4, vo.getSecretYn());
 		int result = pstmt.executeUpdate();
 		
@@ -28,5 +31,106 @@ public class QnaDao {
 		return result;
 		
 	}//write end
+	
+	
+	/**
+	 * 문의글 목록 (최신순) 
+	 * 
+	 * SELECT *
+	 * FROM QNA
+	 * ORDER BY QNA_NO DESC
+	 */
+	public List<QnaVo> qnaList(Connection conn) throws Exception {
+		
+		//sql
+		String sql = "SELECT Q.QNA_NO , Q.TITLE , M.NICK AS WRITER_NICK , Q.HIT , TO_CHAR(Q.MEMBER_ENROLL_DATE, 'YYYY\"년\"MM\"월\"DD\"일\"') AS MEMBER_ENROLL_DATE FROM QNA Q JOIN MEMBER M ON Q.MEMBER_NO = M.MEMBER_NO ORDER BY QNA_NO DESC";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
+		
+		//rs
+		List<QnaVo> voList = new ArrayList<QnaVo>();
+		while(rs.next()) {
+			String qnaNo = rs.getString("QNA_NO");
+			String title = rs.getString("TITLE");
+			String writerNick = rs.getString("WRITER_NICK");
+			String hit = rs.getString("HIT");
+			String enrollDate = rs.getString("MEMBER_ENROLL_DATE");
+			
+			QnaVo vo = new QnaVo();
+			vo.setQnaNo(qnaNo);
+			vo.setTitle(title);
+			vo.setWriterNick(writerNick);
+			vo.setHit(hit);
+			vo.setMemberEnrollDate(enrollDate);
+			
+			voList.add(vo);
+			
+		}
+		
+		//close
+		JDBCTemplate.close(pstmt);
+		JDBCTemplate.close(rs);
+		return voList;
+		
+	}//qnaList end
+	
+	/**
+	 * 문의글 상세조회 (번호) 
+	 * 
+	 * SELECT
+	 * FROM QNA
+	 * WHERE QNA_NO = ?
+	 */
+	public QnaVo qnaDetailByNo(Connection conn, String num) throws Exception {
+		
+		//sql
+		String sql = "SELECT Q.QNA_NO , Q.TITLE , Q.CONTENT , M.NICK AS WRITER_NICK , Q.HIT , TO_CHAR(Q.MEMBER_ENROLL_DATE, 'YYYY\"년\"MM\"월\"DD\"일\"') AS MEMBER_ENROLL_DATE FROM QNA Q JOIN MEMBER M ON Q.MEMBER_NO = M.MEMBER_NO WHERE QNA_NO = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, num);
+		ResultSet rs = pstmt.executeQuery();
+		
+		//rs
+		QnaVo vo = null;
+		if(rs.next()) {
+			String qnaNo = rs.getString("QNA_NO");
+			String title = rs.getString("TITLE");
+			String content = rs.getString("CONTENT");
+			String writerNick = rs.getString("WRITER_NICK");
+			String hit = rs.getString("HIT");
+			String enrollDate = rs.getString("MEMBER_ENROLL_DATE");
+			
+			vo = new QnaVo();
+			vo.setQnaNo(qnaNo);
+			vo.setTitle(title);
+			vo.setContent(content);
+			vo.setWriterNick(writerNick);
+			vo.setHit(hit);
+			vo.setMemberEnrollDate(enrollDate);
+			
+		}
+		
+		//close
+		JDBCTemplate.close(pstmt);
+		JDBCTemplate.close(rs);
+		return vo;
+
+	}//qnaDetailByNo end
+	
+	//조회수 증가
+	public int increaseHit(Connection conn, String num) throws Exception {
+		
+		//sql
+		String sql = "UPDATE QNA SET HIT = HIT + 1 WHERE QNA_NO = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, num);
+		int result = pstmt.executeUpdate();
+		
+		//rs
+		
+		//close
+		JDBCTemplate.close(pstmt);
+		return result;
+		
+	}//increaseHit end
 	
 }
--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/qna/vo/QnaVo.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/qna/vo/QnaVo.java
@@ -2,99 +2,146 @@ package com.cherry.qna.vo;
 
 public class QnaVo {
 	
-	private String no;
+	private String qnaNo;
+	private String memberNo;
+	private String managerNo;
 	private String title;
 	private String content;
-	private String writerNo;
-	private String writerNick;
-	private String managerNo;
-	private String hit;
-	private String enrollDate;
-	private String modifyDate;
+	private String memberEnrollDate;
 	private String secretYn;
+	private String answer;
+	private String managerEnrollDate;
+	private String managerEditDate;
+	private String writerNick;
+	private String hit;
 	
-	public QnaVo() {
-	}
-	
-	public QnaVo(String no, String title, String content, String writerNo, String writerNick, String managerNo,
-			String hit, String enrollDate, String modifyDate, String secretYn) {
-		this.no = no;
-		this.title = title;
-		this.content = content;
-		this.writerNo = writerNo;
-		this.writerNick = writerNick;
+	public QnaVo(String qnaNo, String memberNo, String managerNo, String title, String content, String memberEnrollDate,
+			String secretYn, String answer, String managerEnrollDate, String managerEditDate, String writerNick,
+			String hit) {
+		super();
+		this.qnaNo = qnaNo;
+		this.memberNo = memberNo;
 		this.managerNo = managerNo;
-		this.hit = hit;
-		this.enrollDate = enrollDate;
-		this.modifyDate = modifyDate;
-		this.secretYn = secretYn;
-	}
-	public String getNo() {
-		return no;
-	}
-	public void setNo(String no) {
-		this.no = no;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
 		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
 		this.content = content;
-	}
-	public String getWriterNo() {
-		return writerNo;
-	}
-	public void setWriterNo(String writerNo) {
-		this.writerNo = writerNo;
-	}
-	public String getWriterNick() {
-		return writerNick;
-	}
-	public void setWriterNick(String writerNick) {
+		this.memberEnrollDate = memberEnrollDate;
+		this.secretYn = secretYn;
+		this.answer = answer;
+		this.managerEnrollDate = managerEnrollDate;
+		this.managerEditDate = managerEditDate;
 		this.writerNick = writerNick;
+		this.hit = hit;
 	}
+
+	public QnaVo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getQnaNo() {
+		return qnaNo;
+	}
+
+	public void setQnaNo(String qnaNo) {
+		this.qnaNo = qnaNo;
+	}
+
+	public String getMemberNo() {
+		return memberNo;
+	}
+
+	public void setMemberNo(String memberNo) {
+		this.memberNo = memberNo;
+	}
+
 	public String getManagerNo() {
 		return managerNo;
 	}
+
 	public void setManagerNo(String managerNo) {
 		this.managerNo = managerNo;
 	}
-	public String getHit() {
-		return hit;
+
+	public String getTitle() {
+		return title;
 	}
-	public void setHit(String hit) {
-		this.hit = hit;
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
-	public String getEnrollDate() {
-		return enrollDate;
+
+	public String getContent() {
+		return content;
 	}
-	public void setEnrollDate(String enrollDate) {
-		this.enrollDate = enrollDate;
+
+	public void setContent(String content) {
+		this.content = content;
 	}
-	public String getModifyDate() {
-		return modifyDate;
+
+	public String getMemberEnrollDate() {
+		return memberEnrollDate;
 	}
-	public void setModifyDate(String modifyDate) {
-		this.modifyDate = modifyDate;
+
+	public void setMemberEnrollDate(String memberEnrollDate) {
+		this.memberEnrollDate = memberEnrollDate;
 	}
+
 	public String getSecretYn() {
 		return secretYn;
 	}
+
 	public void setSecretYn(String secretYn) {
 		this.secretYn = secretYn;
 	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public String getManagerEnrollDate() {
+		return managerEnrollDate;
+	}
+
+	public void setManagerEnrollDate(String managerEnrollDate) {
+		this.managerEnrollDate = managerEnrollDate;
+	}
+
+	public String getManagerEditDate() {
+		return managerEditDate;
+	}
+
+	public void setManagerEditDate(String managerEditDate) {
+		this.managerEditDate = managerEditDate;
+	}
+
+	public String getWriterNick() {
+		return writerNick;
+	}
+
+	public void setWriterNick(String writerNick) {
+		this.writerNick = writerNick;
+	}
+
+	public String getHit() {
+		return hit;
+	}
+
+	public void setHit(String hit) {
+		this.hit = hit;
+	}
+
 	@Override
 	public String toString() {
-		return "QnaVo [no=" + no + ", title=" + title + ", content=" + content + ", writerNo=" + writerNo
-				+ ", writerNick=" + writerNick + ", managerNo=" + managerNo + ", hit=" + hit + ", enrollDate="
-				+ enrollDate + ", modifyDate=" + modifyDate + ", secretYn=" + secretYn + "]";
+		return "QnaVo [qnaNo=" + qnaNo + ", memberNo=" + memberNo + ", managerNo=" + managerNo + ", title=" + title
+				+ ", content=" + content + ", memberEnrollDate=" + memberEnrollDate + ", secretYn=" + secretYn
+				+ ", answer=" + answer + ", managerEnrollDate=" + managerEnrollDate + ", managerEditDate="
+				+ managerEditDate + ", writerNick=" + writerNick + ", hit=" + hit + "]";
 	}
+	
 	
 	
 
