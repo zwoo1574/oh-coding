73bbcf038d51fe2ffa75dc15078ed702f8041c79
MJ <douob55@gmail.com>
2023-10-20 17:56:28 +0900

    [기능 생성] 작성자 : 이미진 QnaController QnarService QnaDao QnaVo  기능에 필요한 메소드 추가

--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/qna/controller/QnaController.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/qna/controller/QnaController.java
@@ -7,88 +7,91 @@ import com.cherry.qna.service.QnaService;
 import com.cherry.qna.vo.QnaVo;
 
 public class QnaController {
-	
+
 	private final QnaService service;
 
-	//기본 생성자
+	// 기본 생성자
 	public QnaController() {
 		service = new QnaService();
 	}
-	
+
 	// 메뉴선택
 	public void selectMenu() {
 		System.out.println("======= QnA Menu =======");
-		
+
 		System.out.println("1. 문의글 작성");
 		System.out.println("2. 문의글 목록 (최신순)");
 		System.out.println("3. 문의글 상세조회 (번호)");
-		System.out.println("4. 문의글 내가 쓴것만 보기");
-		System.out.println("5. 문의글 목록 (관리자용)");
-		System.out.println("6. 문의글 검색 (제목)");
+		System.out.println("4. 문의글 제목 검색");
+		System.out.println("5. 내가 작성한 문의글");
+		System.out.println("6. 문의글 목록 (관리자용)");
 		System.out.println("7. 관리자 답변작성");
 		System.out.println("8. 관리자 답변수정");
-		
+
 		String num = Main.SC.nextLine();
-		switch(num) {
-		case "1" : write();break;
-		case "2" : qnaList();break;
-		case "3" : qnaDetailByNo();break;
-//		case "4" : qnaMyList();break;
-//		case "5" : 
-//		case "6" : 
+		switch (num) {
+		case "1":write();break;
+		case "2": qnaList();break;
+		case "3":qnaDetailByNo();break;
+		case "4":searchQnaByTitle();break;
+		case "5":qnaMyList();break;
+//		case "6":qnaListManager();break;
 //		case "7" : 
 //		case "8" : 
 		}
-		
-	}//selectMenu end
-	
-	// 문의글 작성
+
+	}// selectMenu end
+
+	// 1.문의글 작성
 	public void write() {
-		
+
 		try {
 			System.out.println("------- 문의글 작성 -------");
-			
-			//로그인 여부
-			if(Main.loginMember == null) {
+
+			// 로그인 여부
+			if (Main.loginMember == null) {
 				throw new Exception("회원만 문의글 작성이 가능합니다.");
 			}
-			
+
 			// 데이터
 			System.out.print("제목: ");
 			String title = Main.SC.nextLine();
 			System.out.print("내용: ");
 			String content = Main.SC.nextLine();
-			
+			System.out.print("비밀글로 올리시겠습니까? (비밀글을 원하시면 Y를 입력해주세요.)");
+			String secretYn = Main.SC.nextLine();
+
 			QnaVo vo = new QnaVo();
 			vo.setTitle(title);
 			vo.setContent(content);
-			
+			vo.setContent(secretYn);
+
 			// 서비스
 			int result = service.write(vo);
-			
+
 			// 결과
-			if(result != 1) {
+			if (result != 1) {
 				throw new Exception();
 			}
 			System.out.println("문의글 작성 완료 !!!");
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("문의글 작성 실패 ...");
 			e.printStackTrace();
 		}
-			
-	}//write end
-	
-	// 문의글 목록 (최신순)
+
+	}// write end
+
+	// 2.문의글 목록 (최신순)
 	public void qnaList() {
-		
+
 		try {
 			System.out.println("------- 문의글 목록 -------");
-			
+
 			// 데이터
-			
+
 			// 서비스
 			List<QnaVo> voList = service.qnaList();
-			
+
 			// 결과
 			System.out.print("번호");
 			System.out.print(" / ");
@@ -100,8 +103,8 @@ public class QnaController {
 			System.out.print(" / ");
 			System.out.print("작성일자");
 			System.out.println();
-			
-			for(QnaVo vo : voList) {
+
+			for (QnaVo vo : voList) {
 				System.out.print(vo.getQnaNo());
 				System.out.print(" / ");
 				System.out.print(vo.getTitle());
@@ -113,29 +116,29 @@ public class QnaController {
 				System.out.print(vo.getMemberEnrollDate());
 				System.out.println();
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			System.out.println("문의글 목록 조회 실패 ...");
 			e.printStackTrace();
 		}
-		
-	}//qnaList end
-	
-	//문의글 상세조회 (번호)
+
+	}// qnaList end
+
+	// 3.문의글 상세조회 (번호)
 	public void qnaDetailByNo() {
-		
+
 		try {
 			System.out.println("------- 문의글 상세 조회 (번호) -------");
-			
+
 			// 데이터
 			System.out.print("조회할 문의글 번호: ");
 			String num = Main.SC.nextLine();
-			
+
 			// 서비스
 			QnaVo vo = service.qnaDetailByNo(num);
-			
+
 			// 결과
-			if(vo == null) {
+			if (vo == null) {
 				throw new Exception();
 			}
 			System.out.println("---------------------------------------");
@@ -146,26 +149,85 @@ public class QnaController {
 			System.out.println("작성일자: " + vo.getMemberEnrollDate());
 			System.out.println("내용: " + vo.getContent());
 			System.out.println("---------------------------------------");
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			System.out.println("문의글 상세 조회 실패 ...");
 			e.printStackTrace();
 		}
-		
-	}//qnaDetailByNo end
-	
-	//문의글 내가 쓴것만 보기
-//	public void qnaMyList() {
-//
+
+	}// qnaDetailByNo end
+
+	// 4. 문의글 검색 (제목)
+	public void searchQnaByTitle() {
+
+		try {
+			System.out.println("------- 문의글 제목 검색 -------");
+
+			// 데이터
+			System.out.print("검색할 제목: ");
+			String searchValue = Main.SC.nextLine();
+
+			// 서비스
+			List<QnaVo> voList = service.searchQnaByTitle(searchValue);
+
+			// 결과
+			if (voList.size() == 0) {
+				System.out.println("검색 결과가 없습니다.");
+			}
+			for (QnaVo vo : voList) {
+				System.out.println(vo);
+			}
+
+		} catch (Exception e) {
+			System.out.println("게시글 검색 실패 ...");
+			e.printStackTrace();
+		}
+
+	}// searchQnaByTitle end
+
+	// 5. 내가 작성한 문의글
+	public void qnaMyList() {
+
+		try {
+			System.out.println("------- 내가 작성한 문의글 조회 -------");
+			
+			// 로그인 체크
+			if(Main.loginMember == null) {
+				throw new Exception("로그인한 회원만 내 문의글 조회가 가능합니다.");
+			}
+			
+			//데이터
+			String loginMember = Main.loginMember.getMemberNo();
+			
+			//서비스
+			List<QnaVo> voList = service.qnaMyList(loginMember);
+			
+			//결과
+			if(voList.size() == 0) {
+				System.out.println("검색결과가 없습니다.");
+			}
+			for(QnaVo vo : voList) {
+				System.out.println(vo);
+			}
+			
+		}catch(Exception e) {
+			System.out.println("게시글 목록 조회 실패 ...");
+			e.printStackTrace();
+		}
+
+	} // qnaMyList
+
+	// 6. 문의글 목록 (관리자용)
+//	public void qnaListManager() {
 //		try {
-//			System.out.println("------- 내가 쓴 문의글 조회 -------");
+//			System.out.println("------- 문의글 목록 (관리자용) -------");
 //			
-//			//데이터
+//			// 데이터
 //			
-//			//서비스
-//			List<QnaVo> voList = service.qnaMyList();
+//			// 서비스
+//			List<QnaVo> voList = service.qnaListManager();
 //			
-//			//결과
+			// 결과
 //			System.out.print("번호");
 //			System.out.print(" / ");
 //			System.out.print("제목");
@@ -180,17 +242,21 @@ public class QnaController {
 //			for(QnaVo vo : voList) {
 //				System.out.print(vo.getQnaNo());
 //				System.out.print(" / ");
-//				
+//				System.out.print(vo.getTitle());
+//				System.out.print(" / ");
+//				System.out.print(vo.getWriterNick());
+//				System.out.print(" / ");
+//				System.out.print(vo.getHit());
+//				System.out.print(" / ");
+//				System.out.print(vo.getMemberEnrollDate());
+//				System.out.println();
 //			}
 //			
-//			
 //		}catch(Exception e) {
-//			
+//			System.out.println("게시글 목록 조회 실패 ...");
+//			e.printStackTrace();
 //		}
 //		
-//		
-//		
-//	}//qnaMyList
-//	
+//	}//qnaListManger end
 	
 }
--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/qna/dao/QnaDao.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/qna/dao/QnaDao.java
@@ -12,7 +12,7 @@ import com.cherry.qna.vo.QnaVo;
 
 public class QnaDao {
 
-	// 문의글 작성
+	// 1.문의글 작성
 	public int write(Connection conn, QnaVo vo) throws Exception {
 		
 		//sql
@@ -34,7 +34,7 @@ public class QnaDao {
 	
 	
 	/**
-	 * 문의글 목록 (최신순) 
+	 * 2. 문의글 목록 (최신순) 
 	 * 
 	 * SELECT *
 	 * FROM QNA
@@ -43,7 +43,7 @@ public class QnaDao {
 	public List<QnaVo> qnaList(Connection conn) throws Exception {
 		
 		//sql
-		String sql = "SELECT Q.QNA_NO , Q.TITLE , M.NICK AS WRITER_NICK , Q.HIT , TO_CHAR(Q.MEMBER_ENROLL_DATE, 'YYYY\"년\"MM\"월\"DD\"일\"') AS MEMBER_ENROLL_DATE FROM QNA Q JOIN MEMBER M ON Q.MEMBER_NO = M.MEMBER_NO ORDER BY QNA_NO DESC";
+		String sql = "SELECT Q.QNA_NO , Q.TITLE , M.NICK AS WRITER_NICK , Q.HIT , TO_CHAR(Q.MEMBER_ENROLL_DATE, 'YYYY\"년\"MM\"월\"DD\"일\"') AS MEMBER_ENROLL_DATE FROM QNA Q JOIN MEMBER M ON Q.MEMBER_NO = M.MEMBER_NO WHERE SECRET_YN = 'N' ORDER BY QNA_NO DESC";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		ResultSet rs = pstmt.executeQuery();
 		
@@ -75,7 +75,7 @@ public class QnaDao {
 	}//qnaList end
 	
 	/**
-	 * 문의글 상세조회 (번호) 
+	 * 3. 문의글 상세조회 (번호) 
 	 * 
 	 * SELECT
 	 * FROM QNA
@@ -84,7 +84,7 @@ public class QnaDao {
 	public QnaVo qnaDetailByNo(Connection conn, String num) throws Exception {
 		
 		//sql
-		String sql = "SELECT Q.QNA_NO , Q.TITLE , Q.CONTENT , M.NICK AS WRITER_NICK , Q.HIT , TO_CHAR(Q.MEMBER_ENROLL_DATE, 'YYYY\"년\"MM\"월\"DD\"일\"') AS MEMBER_ENROLL_DATE FROM QNA Q JOIN MEMBER M ON Q.MEMBER_NO = M.MEMBER_NO WHERE QNA_NO = ?";
+		String sql = "SELECT Q.QNA_NO , Q.TITLE , Q.CONTENT , M.NICK AS WRITER_NICK , Q.HIT , TO_CHAR(Q.MEMBER_ENROLL_DATE, 'YYYY\"년\"MM\"월\"DD\"일\"') AS MEMBER_ENROLL_DATE FROM QNA Q JOIN MEMBER M ON Q.MEMBER_NO = M.MEMBER_NO WHERE QNA_NO = ? AND SECRET_YN = 'N'";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setString(1, num);
 		ResultSet rs = pstmt.executeQuery();
@@ -116,7 +116,7 @@ public class QnaDao {
 
 	}//qnaDetailByNo end
 	
-	//조회수 증가
+	// 3-2.조회수 증가
 	public int increaseHit(Connection conn, String num) throws Exception {
 		
 		//sql
@@ -132,5 +132,78 @@ public class QnaDao {
 		return result;
 		
 	}//increaseHit end
+	
+	// 4.문의글 검색 (제목)
+	public List<QnaVo> searchQnaByTitle(Connection conn, String searchValue) throws Exception {
+		
+		//sql
+		String sql = "SELECT Q.QNA_NO , Q.TITLE , M.NICK AS WRITER_NICK , Q.HIT , TO_CHAR(Q.MEMBER_ENROLL_DATE, 'YYYY\"년\"MM\"월\"DD\"일\"') AS ENROLL_DATE FROM QNA Q JOIN MEMBER M ON Q.MEMBER_NO = M.MEMBER_NO WHERE Q.TITLE LIKE '%'||?||'%' AND Q.SECRET_YN = 'N' ORDER BY Q.QNA_NO DESC";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, searchValue);
+		ResultSet rs = pstmt.executeQuery();
+		
+		//rs
+		List<QnaVo> voList = new ArrayList<QnaVo> ();
+		while(rs.next()) {
+			String no = rs.getString("QNA_NO");
+			String title = rs.getString("TITLE");
+			String writerNick = rs.getString("WRITER_NICK");
+			String hit = rs.getString("HIT");
+			String enrollDate = rs.getString("ENROLL_DATE");
+			
+			QnaVo vo = new QnaVo();
+			vo.setQnaNo(no);
+			vo.setTitle(title);
+			vo.setWriterNick(writerNick);
+			vo.setHit(hit);
+			vo.setMemberEnrollDate(enrollDate);
+			
+			voList.add(vo);
+			
+		}
+		
+		//close
+		JDBCTemplate.close(pstmt);
+		JDBCTemplate.close(rs);
+		return voList;
+		
+	}//searchQnaByTitle end
+
+	// 5. 내가 작성한 문의글
+	public List<QnaVo> qnaMyList(Connection conn, String loginMember) throws Exception {
+		
+		// sql
+		String sql = "SELECT Q.QNA_NO , Q.TITLE , M.NICK AS WRITER_NICK , Q.HIT , TO_CHAR(Q.MEMBER_ENROLL_DATE, 'YYYY\"년\"MM\"월\"DD\"일\"') AS ENROLL_DATE FROM QNA Q JOIN MEMBER M ON Q.MEMBER_NO = M.MEMBER_NO WHERE M.MEMBER_NO LIKE ? ORDER BY Q.QNA_NO DESC";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, loginMember);
+		ResultSet rs = pstmt.executeQuery();
+		
+		// rs
+		List<QnaVo> voList = new ArrayList<QnaVo>();
+		while(rs.next()) {
+			String no = rs.getString("QNA_NO");
+			String title = rs.getString("TITLE");
+			String nick = rs.getString("WRITER_NICK");
+			String hit = rs.getString("HIT");
+			String enrollDate = rs.getString("ENROLL_DATE");
+			
+			QnaVo vo = new QnaVo();
+			vo.setQnaNo(no);
+			vo.setTitle(title);
+			vo.setWriterNick(nick);
+			vo.setHit(hit);
+			vo.setMemberEnrollDate(enrollDate);
+			
+			voList.add(vo);
+			
+		}
+		
+		// close
+		JDBCTemplate.close(pstmt);
+		JDBCTemplate.close(rs);
+		return voList;
+		
+	}//qnaMyList end
+	
 	
 }
