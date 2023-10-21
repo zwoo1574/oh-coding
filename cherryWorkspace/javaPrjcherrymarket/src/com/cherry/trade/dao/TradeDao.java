35187cd1113c14deae6a59e8a553e74fd3997a6b
JB <whdqja918@gmail.com>
2023-10-21 18:06:47 +0900

    [ Trade 게시판 ] 박종범 게시글 삭제 추가 조회수 추가 TradeVo hit 추가

--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/trade/dao/TradeDao.java
+++ cherryWorkspace/javaPrjcherrymarket/src/com/cherry/trade/dao/TradeDao.java
@@ -17,8 +17,9 @@ public class TradeDao {
 
 	// 게시글 목록 (중고거래 메인)
 	public List<TradeVo> printPost(Connection conn) throws Exception {
-
-		String sql = "SELECT RPAD(T.BOARD_NO, 20, ' ') BOARD_NO ,RPAD(T.TITLE, 60, ' ') TITLE ,RPAD(T.PRODUCT, 40, ' ') PRODUCT ,RPAD(T.PRICE, 20, ' ') PRICE ,RPAD(M.NICK, 20, ' ') NICK ,RPAD(TO_CHAR(T.ENROLL_DATE, 'YYYY-MM-DD'), 20, ' ') ENROLL_DATE ,T.PRODUCT ,T.PRICE FROM TRADE T JOIN MEMBER M ON T.MEMBER_NO = M.MEMBER_NO WHERE DEL_YN = 'N' ORDER BY ENROLL_DATE DESC";
+//             "%-30s%-50s%-30s%-30s%-30s%-30s\n", "번호", "제목", "물품명", "가격", "판매자", "날짜"
+		String sql = "SELECT RPAD(T.BOARD_NO, 30, ' ') BOARD_NO ,RPAD(T.TITLE, 50, ' ') TITLE ,RPAD(T.PRODUCT, 30, ' ') PRODUCT ,RPAD(T.PRICE, 30, ' ') PRICE ,RPAD(M.NICK, 30, ' ') NICK ,RPAD(TO_CHAR(T.ENROLL_DATE, 'YYYY-MM-DD'), 30, ' ') ENROLL_DATE ,T.PRODUCT ,T.PRICE, T.HIT, T.MEMBER_NO FROM TRADE T JOIN MEMBER M ON T.MEMBER_NO = M.MEMBER_NO WHERE T.DEL_YN = 'N' AND T.COMPLETE_YN = 'N' ORDER BY ENROLL_DATE DESC";
+//		String sql = "SELECT T.BOARD_NO ,T.TITLE ,T.PRODUCT ,T.PRICE ,M.NICK ,TO_CHAR(T.ENROLL_DATE, 'YYYY-MM-DD') ENROLL_DATE ,T.PRODUCT ,T.PRICE FROM TRADE T JOIN MEMBER M ON T.MEMBER_NO = M.MEMBER_NO WHERE DEL_YN = 'N' ORDER BY ENROLL_DATE DESC";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		ResultSet rs = pstmt.executeQuery();
 		
@@ -30,6 +31,8 @@ public class TradeDao {
 			String price = rs.getString("PRICE");
 			String nick = rs.getString("NICK");
 			String date = rs.getString("ENROLL_DATE");
+			String hit = rs.getString("HIT");
+			String memberNo = rs.getString("MEMBER_NO");
 			
 			TradeVo vo = new TradeVo();
 			vo.setBoardNo(no);
@@ -38,6 +41,8 @@ public class TradeDao {
 			vo.setPrice(price);
 			vo.setMemberNick(nick);
 			vo.setEnrollDate(date);
+			vo.setHit(hit);
+			vo.setMemberNo(memberNo);
 			
 			voList.add(vo);
 		}
@@ -51,17 +56,14 @@ public class TradeDao {
 	// 게시글 조회   -- 조회수 추가해야함
 	public TradeVo showContent(String select, Connection conn) throws Exception {
 		
-		String sql = "SELECT T.BOARD_NO, T.TITLE, T.CONTENT, T.TRADE_AREAS, T.PRODUCT, T.PRICE, T.ENROLL_DATE, T.EDIT_DATE, M.NICK, A.AREAS_NAME FROM TRADE T JOIN MEMBER M ON T.MEMBER_NO = M.MEMBER_NO JOIN AREAS A ON M.AREAS_CODE = A.AREAS_CODE WHERE T.BOARD_NO = ? AND T.COMPLETE_YN = 'N' AND T.DEL_YN = 'N'";
-		String hit = "UPDATE TRADE SET HIT = HIT +1 WHERE BOARD_NO = ? ";
+		String sql = "SELECT T.BOARD_NO, T.TITLE, T.CONTENT, T.TRADE_AREAS, T.PRODUCT, T.PRICE, T.ENROLL_DATE, T.EDIT_DATE, M.NICK, A.AREAS_NAME, T.HIT FROM TRADE T JOIN MEMBER M ON T.MEMBER_NO = M.MEMBER_NO JOIN AREAS A ON M.AREAS_CODE = A.AREAS_CODE WHERE T.BOARD_NO = ? AND T.COMPLETE_YN = 'N' AND T.DEL_YN = 'N'";
+		
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-		PreparedStatement pstmt2 = conn.prepareStatement(hit);		
 		pstmt.setString(1, select);
-		pstmt2.setString(1, select);
-		pstmt2.executeUpdate();
+		
 		ResultSet rs = pstmt.executeQuery();
 		
 		TradeVo vo = null;
-		
 		if(rs.next()) {
 			String boardNo = rs.getString("BOARD_NO");
 			String title = rs.getString("TITLE");
@@ -73,6 +75,7 @@ public class TradeDao {
 			String price = rs.getString("PRICE");
 			String enrollDate = rs.getString("ENROLL_DATE");
 			String editDate = rs.getString("EDIT_DATE");
+			String hit = rs.getString("HIT");
 			
 			vo = new TradeVo();
 			vo.setBoardNo(boardNo);
@@ -85,6 +88,7 @@ public class TradeDao {
 			vo.setPrice(price);
 			vo.setEnrollDate(enrollDate);
 			vo.setEditDate(editDate);
+			vo.setHit(hit);
 		}
 			
 		JDBCTemplate.close(pstmt);
@@ -99,6 +103,7 @@ public class TradeDao {
 		String sql = "UPDATE TRADE SET HIT = HIT + 1 WHERE BOARD_NO = ? AND MEMBER_NO != ?";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setString(1, select);
+		
 		if(Main.loginMember != null) {
 			pstmt.setString(2, Main.loginMember.getMemberNo());
 		} else {
@@ -121,9 +126,25 @@ public class TradeDao {
 		pstmt.setString(2, vo.getProduct());
 		pstmt.setString(3, vo.getPrice());
 		pstmt.setString(4, vo.getTradeAreas());
-		pstmt.setString(5, vo.getContent());
 		pstmt.setString(6, vo.getBoardNo());
 
+		
+		String content = vo.getContent();
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < content.length(); i++) {
+			char ch = content.charAt(i);
+			sb.append(ch);
+			if((i+1) % 20 == 0 && i != 0) {
+				sb.append("#");
+			}
+		}
+		
+		content = sb.toString();
+		
+		pstmt.setString(5, content);
+		
+		
 		int result = pstmt.executeUpdate();
 		
 		JDBCTemplate.close(pstmt);
@@ -166,12 +187,18 @@ public class TradeDao {
 		return result;
 	}
 
-	// 게시글 삭제     -- 미완성
-	public int delPost(String del, Connection conn) {
+	// 게시글 삭제 
+	public int delPost(String boardNo, Connection conn) throws Exception {
 		
+		String sql ="UPDATE TRADE SET DEL_YN = 'Y' WHERE BOARD_NO = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, boardNo);
 		
+		int result = pstmt.executeUpdate();
 		
-		return 0;
+		JDBCTemplate.close(pstmt);
+		
+		return result;
 	}
 
 	// 관심목록 추가 
@@ -253,6 +280,8 @@ public class TradeDao {
 			pstmt2.setString(2, vo.getBoardNo());
 			
 			pstmt2.executeUpdate();
+			
+			JDBCTemplate.close(pstmt2);
 		}
 		
 		JDBCTemplate.close(pstmt);
