5811ec0252947c9dd0a2259d7211a910c6ecfc6f
Kwangs <leekp7@naver.com>
2023-10-25 20:36:37 +0900

    [FAQ] 작성자: 이광포
    FaqController
    FaqService
    Main.test 다 지움
    
    최종 푸쉬?? 젭알!

--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/main/test/LhmMain.java
+++
@@ -1,9 +0,0 @@
-package com.cherry.main.test;
-
-public class LhmMain {
-
-	public static void main(String[] args) {
-
-	}
-
-}
--- cherryWorkspace/javaPrjcherrymarket/src/com/cherry/main/test/LkpMain.java
+++
@@ -1,9 +0,0 @@
-package com.cherry.main.test;
-
-public class LkpMain {
-
-	public static void main(String[] args) {
-
-	}
-
-}
